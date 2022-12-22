9bd2b262b9e81ddcdd807fcb7d9423f0ca4825a6
Lucas Rueda <lucasrueda64@gmail.com>
2022-12-22 20:39:52 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const { Server } = require('socket.io');
+const routerViews = require('./routes/views.router.js');
+
+// configurar servidores
+const app = express();
+const httpServer = app.listen(8080, ()=> console.log('listening'));
+const io = new Server(httpServer);
+
+// config engine templates
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+app.use('/', routerViews);
+
+
+const messages = []
+
+io.on('connection', socket =>{
+    console.log('new cliente connected');
+
+    socket.on('message', data =>{
+        messages.push(data)
+        io.emit('logs', messages)
+    })
+})
